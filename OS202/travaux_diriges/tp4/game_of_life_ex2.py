6a4d5cc69aeaad3a94d83ffdec6e4d40f0a5ecac
YasBroek <yasmin.van-den-broek@ensta-paris.fr>
2025-03-03 19:18:05 +0100

    tp4

---
+++ OS202/travaux_diriges/tp4/game_of_life_ex2.py
@@ -0,0 +1,132 @@
+"""
+Le jeu de la vie
+################
+Le jeu de la vie est un automate cellulaire inventé par Conway se basant normalement sur une grille infinie
+de cellules en deux dimensions. Ces cellules peuvent prendre deux états :
+    - un état vivant
+    - un état mort
+A l'initialisation, certaines cellules sont vivantes, d'autres mortes.
+Le principe du jeu est alors d'itérer de telle sorte qu'à chaque itération, une cellule va devoir interagir avec
+les huit cellules voisines (gauche, droite, bas, haut et les quatre en diagonales.) L'interaction se fait selon les
+règles suivantes pour calculer l'irération suivante :
+    - Une cellule vivante avec moins de deux cellules voisines vivantes meurt ( sous-population )
+    - Une cellule vivante avec deux ou trois cellules voisines vivantes reste vivante
+    - Une cellule vivante avec plus de trois cellules voisines vivantes meurt ( sur-population )
+    - Une cellule morte avec exactement trois cellules voisines vivantes devient vivante ( reproduction )
+
+Pour ce projet, on change légèrement les règles en transformant la grille infinie en un tore contenant un
+nombre fini de cellules. Les cellules les plus à gauche ont pour voisines les cellules les plus à droite
+et inversement, et de même les cellules les plus en haut ont pour voisines les cellules les plus en bas
+et inversement.
+
+On itère ensuite pour étudier la façon dont évolue la population des cellules sur la grille.
+"""
+import pygame as pg
+import numpy as np
+from mpi4py import MPI
+import time
+import sys
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+class Grille:
+    def __init__(self, dim, init_pattern=None, color_life=pg.Color("black"), color_dead=pg.Color("white")):
+        self.dimensions = dim
+        self.cells = np.zeros(self.dimensions, dtype=np.uint8)
+        if init_pattern is not None:
+            indices_i = [v[0] for v in init_pattern]
+            indices_j = [v[1] for v in init_pattern]
+            self.cells[indices_i, indices_j] = 1
+        else:
+            self.cells = np.random.randint(2, size=dim, dtype=np.uint8)
+        self.col_life = color_life
+        self.col_dead = color_dead
+
+    def compute_next_iteration(self):
+        neighbours_count = sum(
+            np.roll(np.roll(self.cells, i, 0), j, 1)
+            for i in (-1, 0, 1) for j in (-1, 0, 1) if (i != 0 or j != 0)
+        )
+        next_cells = (neighbours_count == 3) | (self.cells & (neighbours_count == 2))
+        self.cells = next_cells
+
+class App:
+    def __init__(self, geometry, grid):
+        self.grid = grid
+        self.size_x = geometry[1] // grid.dimensions[1]
+        self.size_y = geometry[0] // grid.dimensions[0]
+        self.width = grid.dimensions[1] * self.size_x
+        self.height = grid.dimensions[0] * self.size_y
+        self.screen = pg.display.set_mode((self.width, self.height))
+        self.colors = np.array([self.grid.col_dead[:-1], self.grid.col_life[:-1]])
+
+    def draw(self):
+        surface = pg.surfarray.make_surface(self.colors[self.grid.cells.T])
+        surface = pg.transform.flip(surface, False, True)
+        surface = pg.transform.scale(surface, (self.width, self.height))
+        self.screen.blit(surface, (0, 0))
+        pg.display.update()
+
+if __name__ == '__main__':
+    pg.init()
+    
+    dico_patterns = { # Dimension et pattern dans un tuple
+        'blinker' : ((5,5),[(2,1),(2,2),(2,3)]),
+        'toad'    : ((6,6),[(2,2),(2,3),(2,4),(3,3),(3,4),(3,5)]),
+        "acorn"   : ((100,100), [(51,52),(52,54),(53,51),(53,52),(53,55),(53,56),(53,57)]),
+        "beacon"  : ((6,6), [(1,3),(1,4),(2,3),(2,4),(3,1),(3,2),(4,1),(4,2)]),
+        "boat" : ((5,5),[(1,1),(1,2),(2,1),(2,3),(3,2)]),
+        "glider": ((100,90),[(1,1),(2,2),(2,3),(3,1),(3,2)]),
+        "glider_gun": ((200,100),[(51,76),(52,74),(52,76),(53,64),(53,65),(53,72),(53,73),(53,86),(53,87),(54,63),(54,67),(54,72),(54,73),(54,86),(54,87),(55,52),(55,53),(55,62),(55,68),(55,72),(55,73),(56,52),(56,53),(56,62),(56,66),(56,68),(56,69),(56,74),(56,76),(57,62),(57,68),(57,76),(58,63),(58,67),(59,64),(59,65)]),
+        "space_ship": ((25,25),[(11,13),(11,14),(12,11),(12,12),(12,14),(12,15),(13,11),(13,12),(13,13),(13,14),(14,12),(14,13)]),
+        "die_hard" : ((100,100), [(51,57),(52,51),(52,52),(53,52),(53,56),(53,57),(53,58)]),
+        "pulsar": ((17,17),[(2,4),(2,5),(2,6),(7,4),(7,5),(7,6),(9,4),(9,5),(9,6),(14,4),(14,5),(14,6),(2,10),(2,11),(2,12),(7,10),(7,11),(7,12),(9,10),(9,11),(9,12),(14,10),(14,11),(14,12),(4,2),(5,2),(6,2),(4,7),(5,7),(6,7),(4,9),(5,9),(6,9),(4,14),(5,14),(6,14),(10,2),(11,2),(12,2),(10,7),(11,7),(12,7),(10,9),(11,9),(12,9),(10,14),(11,14),(12,14)]),
+        "floraison" : ((40,40), [(19,18),(19,19),(19,20),(20,17),(20,19),(20,21),(21,18),(21,19),(21,20)]),
+        "block_switch_engine" : ((400,400), [(201,202),(201,203),(202,202),(202,203),(211,203),(212,204),(212,202),(214,204),(214,201),(215,201),(215,202),(216,201)]),
+        "u" : ((200,200), [(101,101),(102,102),(103,102),(103,101),(104,103),(105,103),(105,102),(105,101),(105,105),(103,105),(102,105),(101,105),(101,104)]),
+        "flat" : ((200,400), [(80,200),(81,200),(82,200),(83,200),(84,200),(85,200),(86,200),(87,200), (89,200),(90,200),(91,200),(92,200),(93,200),(97,200),(98,200),(99,200),(106,200),(107,200),(108,200),(109,200),(110,200),(111,200),(112,200),(114,200),(115,200),(116,200),(117,200),(118,200)])
+    }
+    
+    choice = 'glider'
+    resx, resy = 800, 800
+    
+    try:
+        init_pattern = dico_patterns[choice]
+    except KeyError:
+        print("No such pattern. Available ones are:", dico_patterns.keys())
+        exit(1)
+    
+    grid = Grille(*init_pattern)
+    
+    if rank == 0:
+        appli = App((resx, resy), grid)
+    
+    loop = True
+    while loop:
+        t1 = time.time()
+        
+        grid.compute_next_iteration()
+        
+        if rank == 1:
+            comm.send(grid.cells[:, 0:400], dest=0, tag=1)
+        elif rank == 2:
+            comm.send(grid.cells[:, 400:800], dest=0, tag=2)
+        elif rank == 0:
+            g1 = comm.recv(source=1, tag=1)
+            g2 = comm.recv(source=2, tag=2)
+            grid.cells[:, 0:400] = g1
+            grid.cells[:, 400:800] = g2
+            
+            appli.draw()
+        
+        t2 = time.time()
+        t3 = time.time()
+        print(f"Temps calcul: {t2-t1:2.2e} s, affichage: {t3-t2:2.2e} s", end='\r')
+        
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                loop = False
+
+    pg.quit()
